44677b4363613314fae1099aa288ef152769ddc6
Kyle Kukkola <kkukkola33@gmail.com>
2022-02-17 13:55:15 -0800

    now using quill editor for the notepad custom function for the notepad resizing

--- renderer-process/Notepad.js
+++ renderer-process/Notepad.js
@@ -8,22 +8,22 @@ Notepad.Refresh = () => {
 
 Notepad.Open = function(filepath) {
     console.log("Notepad.Open")
-    fs.ReadFile(filepath, function(text) {
-        console.log("this is the text:", text)
-        Notepad.currentFile = filepath
-        $('#notepad-textarea').val(text)
-    })
+    // fs.ReadFile(filepath, function(text) {
+    //     console.log("this is the text:", text)
+    //     Notepad.currentFile = filepath
+    //     $('#notepad-textarea').val(text)
+    // })
 }
 
 Notepad.Save = function() {
     console.log("Notepad.Save")
-    if (Notepad.currentFile !== null) {
-        let text = $('#notepad-textarea').val() 
-        console.log("Tet=xt: ", text)
-        fs.OverwriteFile(Notepad.currentFile, text, function() {
-            console.log("Successfull save!")
-        })
-    }
+    // if (Notepad.currentFile !== null) {
+    //     let text = $('#notepad-textarea').val() 
+    //     console.log("Tet=xt: ", text)
+    //     fs.OverwriteFile(Notepad.currentFile, text, function() {
+    //         console.log("Successfull save!")
+    //     })
+    // }
 }
 
 export {Notepad}
