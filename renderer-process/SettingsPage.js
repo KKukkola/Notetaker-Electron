9a0b6e485e5f24a585ae89451758986dbed66a22
Kyle Kukkola <kkukkola33@gmail.com>
2022-04-01 10:21:56 -0700

    settings page displays which theme is currently chosen

--- renderer-process/SettingsPage.js
+++ renderer-process/SettingsPage.js
@@ -1,6 +1,9 @@
 
 let SettingsPage = new Object()
 
+var $light_theme_button = $('#settings-light-theme-button');
+var $dark_theme_button = $('#settings-dark-theme-button');
+
 $("#to-settingspage").click(function(event) {
     SettingsPage.Show()
 })
@@ -9,18 +12,28 @@ $('#settings-home-button').click(function(event) {
     $("#settingspage").removeClass('isActive')
 })
 
-$("#settings-light-theme-button").click(function(event) {
+$light_theme_button.click(function(event) {
     $('html').addClass("light-theme")
     $('html').removeClass("dark-theme")
+    $dark_theme_button.removeClass("chosen")
+    $light_theme_button.addClass("chosen")
 })
 
-$("#settings-dark-theme-button").click(function(event) {
+$dark_theme_button.click(function(event) {
     $('html').addClass("dark-theme")
     $('html').removeClass("light-theme")
+    $light_theme_button.removeClass("chosen")
+    $dark_theme_button.addClass("chosen")
 })
 
 SettingsPage.Show = function() {
+    if ($('html').hasClass('light-theme')) {
+        $light_theme_button.addClass("chosen")
+    } else {
+        $dark_theme_button.addClass('chosen')
+    }
     $("#settingspage").addClass('isActive')
+
 }
 
 export {SettingsPage}
